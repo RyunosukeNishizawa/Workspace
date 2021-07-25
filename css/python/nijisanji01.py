516339c1773186c193c83b765410447a29936077
RyunosukeNishizawa <86883911+RyunosukeNishizawa@users.noreply.github.com>
2021-07-25 18:32:06 +0900

    working

--- css/python/nijisanji01.py
+++ css/python/nijisanji01.py
@@ -1,28 +1,65 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import json
-
+import pandas as pd
 from apiclient.discovery import build
+from apiclient.errors import HttpError
 
 API_KEY = 'AIzaSyBoaa14uzdor3FZ6KjaZOUsUrgPxu6EG6o'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 CHANNEL_ID = 'UCD-miitqNY3nyukJ4Fnf4_A'
+channels = [] #チャンネル情報を格納する配列
+searches = [] #videoidを格納する配列
+videos = [] #各動画情報を格納する配列
+nextPagetoken = None
+nextpagetoken = None
 
 youtube = build(
     YOUTUBE_API_SERVICE_NAME,
     YOUTUBE_API_VERSION,
     developerKey=API_KEY
-)
+    )
 
-response = youtube.search().list(
-    part = "id",
-    channelId = CHANNEL_ID,
-    maxResults = 15,
-    order = "date" #日付順にソート
+channel_response = youtube.channels().list(
+    part = 'snippet,statistics',
+    id = CHANNEL_ID
     ).execute()
 
+for channel_result in channel_response.get("items", []):
+    if channel_result["kind"] == "youtube#channel":
+        channels.append([channel_result["snippet"]["title"],channel_result["statistics"]["subscriberCount"],channel_result["statistics"]["videoCount"],channel_result["snippet"]["publishedAt"]])
 
-with open("response.json", mode="w", encoding="utf-8") as f:
-    json.dump(response, f, ensure_ascii=False, indent=2)
+while True:
+    if nextPagetoken != None:
+        nextpagetoken = nextPagetoken
+
+    search_response = youtube.search().list(
+      part = "snippet",
+      channelId = CHANNEL_ID,
+      maxResults = 50,
+      order = "date", #日付順にソート
+      pageToken = nextpagetoken #再帰的に指定
+      ).execute()
+
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            searches.append(search_result["id"]["videoId"])
+
+    try:
+        nextPagetoken =  search_response["nextPageToken"]
+    except:
+        break
+
+for result in searches:
+    video_response = youtube.videos().list(
+      part = 'snippet,statistics',
+      id = result
+      ).execute()
+
+    for video_result in video_response.get("items", []):
+        if video_result["kind"] == "youtube#video":
+            videos.append([video_result["snippet"]["title"],video_result["statistics"]["viewCount"],video_result["statistics"]["likeCount"],video_result["statistics"]["dislikeCount"],video_result["snippet"]["publishedAt"]])
+
+videos_report = pd.DataFrame(videos, columns=['title', 'viewCount', 'likeCount', 'dislikeCount', 'publishedAt'])
+videos_report.to_csv("videos_report.csv", index=None)
+
+channel_report = pd.DataFrame(channels, columns=['title', 'subscriberCount', 'videoCount', 'publishedAt'])
+channel_report.to_csv("channels_report.csv", index=None)
